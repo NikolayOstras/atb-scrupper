a7f12b713c3e8cae96074239b30d906590666cdd
NikolayOstras <nikki.ice.promo@gmail.com>
2024-08-18 15:38:59 +0300

    update versions of puppeteer

--- api/WebBrowser.js
+++ api/WebBrowser.js
@@ -1,8 +1,11 @@
-import chromium from '@sparticuz/chromium'
+import chromium from 'chrome-aws-lambda'
 import puppeteer from 'puppeteer-extra'
 import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
 puppeteer.use(StealthPlugin())
+
+const LOCAL_CHROME_EXECUTABLE =
+	'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
 
 class WebBrowser {
 	constructor() {
@@ -14,14 +17,17 @@ class WebBrowser {
 		this.browser = await puppeteer.launch({
 			args: chromium.args,
 			defaultViewport: chromium.defaultViewport,
-			executablePath: await chromium.executablePath(),
+			executablePath:
+				(await chromium.executablePath) || LOCAL_CHROME_EXECUTABLE,
 			headless: chromium.headless,
 			ignoreHTTPSErrors: true,
 		})
+
 		const pages = await this.browser.pages()
-		if (pages) {
+		if (pages.length > 0) {
 			this.page = pages[0]
 		}
+
 		await this.page.setRequestInterception(true)
 		this.page.on('request', async request => {
 			const resourceType = request.resourceType()
@@ -38,7 +44,9 @@ class WebBrowser {
 	}
 
 	async close() {
-		await this.browser.close()
+		if (this.browser !== null) {
+			await this.browser.close()
+		}
 	}
 
 	async fetchPageHtml(url) {
